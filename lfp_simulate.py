7e93919cfddaf35d22ac7f463556ac1777f5818a
Attar <93458132+att-ar@users.noreply.github.com>
2022-08-17 14:01:49 -0400

    Update lfp_simulate.py

--- lfp_simulate.py
+++ lfp_simulate.py
@@ -15,7 +15,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
 
-from rolling_and_plot import normalize, rolling_split, validate
+from rolling_and_plot import *
 
 st.title("Lithium-Ion Cell Simulator Using the 2nd Order Equivalent Circuit Model")
 st.markdown(
